72ec7e2ce7b9f0114dbd8c79196f4b03c91cfa52
jlengstorf <jason.lengstorf@copterlabs.com>
2015-10-14 16:26:23 +0700

    feat(five-stages): an interactive toy for The Slog post

---
+++ source/scripts/five-stages.js
@@ -0,0 +1,44 @@
+(() => {
+  'use strict';
+
+  const $fiveStages = document.querySelectorAll('.five-stages');
+
+  if ($fiveStages) {
+
+    // Sets up a check to determine whether or not the element is on-screen
+    [].forEach.call($fiveStages, el => {
+
+      // Adds a class to the element so we know JS is working
+      el.classList.add('js__active');
+
+      // Sets up a handler and attaches it to the scroll event
+      const viewportChangeHandler = onVisibilityChange(el);
+      addEventListener('scroll', viewportChangeHandler, false);
+    });
+
+    // Adds a class to on-screen elements
+    function onVisibilityChange(el, callback) {
+      return function () {
+        if (isElementInViewport(el)) {
+          el.classList.add('js__in-viewport');
+        } else {
+          el.classList.remove('js__in-viewport');
+        }
+      }
+    }
+
+    // Helper for checking if the element is within the viewport
+    // @see http://stackoverflow.com/a/7557433/463471
+    function isElementInViewport(el) {
+      const position = el.getBoundingClientRect();
+
+      return (
+        position.top >= 0 &&
+        position.left >= 0 &&
+        position.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        position.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    }
+  }
+
+})();
